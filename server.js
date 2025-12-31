2bbe45fb18cae3960a84b42deeb0d4ab2724f5c7
Ayush Srivastava <ayush261102@hotmail.com>
2026-01-01 00:50:01 +0530

    script updated

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const authRoutes = require('./routes/authRoutes');
 const emailRoutes = require('./routes/emailRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
 
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-dotenv.config();
 
 app.use(cors());
 app.use(express.json());
@@ -22,12 +22,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to Synaptix AI Backend');
 });
 
-// Export for Vercel serverless
-module.exports = app;
-
-// Local development server
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-}
+// Always start server (Render requires this)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
